578cdec25d0c8ed6aaf509c12300476754942be7
Lucas Segalla <lucaswsegalla@gmail.com>
2026-08-24 15:27:31 -0300

    .

--- 5fingers_5servos.ino
+++ 5fingers_5servos.ino
@@ -1,40 +1,21 @@
 #include <Servo.h>
 
 Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-Servo servo5;
 
+int pinoPotenciometro = A0;
+int valorPotenciometro;
+int angulo;
 
 void setup() {
   servo1.attach(9);
-  servo2.attach(10);
-  servo3.attach(11);
-  servo4.attach(12);
-  servo5.attach(13);
-
 }
 
 void loop() {
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(0);
-  servo4.write(0);
-  servo5.write(0);
-  delay(1000);
+  valorPotenciometro = analogRead(pinoPotenciometro);
 
-  servo1.write(90);
-  servo2.write(90);
-  servo3.write(90);
-  servo4.write(90);
-  servo5.write(90);
-  delay(1000);
+  angulo = map(valorPotenciometro, 0, 1023, 20, 160);
 
-  servo1.write(180);
-  servo2.write(180);
-  servo3.write(180);
-  servo4.write(180);
-  servo5.write(180);
-  delay(1000);
+  servo1.write(angulo);
+
+  delay(15);
 }
